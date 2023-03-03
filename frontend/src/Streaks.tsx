0ea157f40d21899311351d9e73e5592ca054fb31
yukinoyu <56956339+yukinoyuu@users.noreply.github.com>
2023-03-02 22:47:26 -0600

    basic streak jsx (testing, for editing styles later)

--- frontend/src/Streaks.tsx
+++ frontend/src/Streaks.tsx
@@ -1,5 +1,29 @@
 const Streaks = () => {
 
+  const getStreaks = () => {
+    const StreaksJSON = fetch(
+      'localhost:3000',
+      {
+        method: 'GET'
+      }
+    )
+    return JSON.stringify(StreaksJSON)
+  }
+
+  const PrototypeOfStreaks = [{streakName: "StreakTest", streak: 10}, {streakName: "Streak test No2", streak: 2}]
+
+  const StreakElements = PrototypeOfStreaks.map((Streak) => {
+    return(
+      <div key={Streak.streakName}
+           className="streak"
+      >
+        <div className="streak-name">{Streak.streakName}</div>
+        <div className="streak-number">{Streak.streak}</div>
+      </div>
+    )
+  })
+
+  return <>{StreakElements}</>
 }
 
 export default Streaks
