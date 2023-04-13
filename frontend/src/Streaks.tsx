3222039c0deb54e082b22b46d867f2d954367290
yukinoyu <56956339+yukinoyuu@users.noreply.github.com>
2023-04-13 14:51:06 -0500

    basic github style styles for streak display pop-up / modal

--- frontend/src/Streaks.tsx
+++ frontend/src/Streaks.tsx
@@ -194,7 +194,7 @@ const Streaks = () => {
       </div>
     </div> } 
     { streakDisplayModal && 
-      <div className="streakDisplayModal"
+      <div className="streakDisplayModal frosted"
         onClick={(e) => {
           if (e.target == e.currentTarget) {
             setStreakDisplayModal(false)
@@ -208,9 +208,10 @@ const Streaks = () => {
         }}
       >
         <div className="streakDisplayBox">
-          <div className="displayBox">
-            <div className="name">{streakDisplayName.current}</div>
+          <div className="cross" style={{display: 'flex', justifyContent: 'right'}} >
+            <svg fill="#ffffff" onClick={() => { setStreakDisplayModal(false) }} height="12px" width="12px" version="1.1" id="Capa_1" viewBox="0 0 460.775 460.775" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path> </g></svg>
           </div>
+          <div className="name" style={{fontSize: '25px', fontWeight: 'bold'}}>{streakDisplayName.current}</div>
         </div>
       </div>
     }
