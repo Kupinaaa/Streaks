fe3dc9893e1a2c85c756ae3710ada0ab1cc39b44
yukinoyu <56956339+yukinoyuu@users.noreply.github.com>
2023-04-26 20:58:44 -0500

    Minor fixes related to onKeyDown and tabIndex

--- frontend/src/Streaks.tsx
+++ frontend/src/Streaks.tsx
@@ -176,7 +176,7 @@ const Streaks = () => {
       }}
       onKeyDown={(e) => {
         if(e.key == 'Enter' || e.key == 'Escape'){
-          addNewStreak(newStreakName)
+          if (e.key == 'Enter') addNewStreak(newStreakName)
           setNewStreakModal(false)
           setNewStreakName('')
         }
@@ -200,16 +200,14 @@ const Streaks = () => {
             setStreakDisplayModal(false)
           } 
         }}
+        tabIndex={0}
         onKeyDown={(e) => {
-          if(e.key == 'Enter' || e.key == 'Escape'){
-            setStreakDisplayModal(false)
-            console.log(e.key)
-          }
+          if(e.key == 'Escape' || e.key == 'Enter') setStreakDisplayModal(false)
         }}
       >
         <div className="streakDisplayBox">
           <div className="cross" style={{display: 'flex', justifyContent: 'right'}} >
-            <svg fill="#ffffff" onClick={() => { setStreakDisplayModal(false) }} height="12px" width="12px" version="1.1" id="Capa_1" viewBox="0 0 460.775 460.775" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path> </g></svg>
+            <svg fill="#ffffff" onClick={() => { setStreakDisplayModal(false) }} height="12px" width="12px" version="1.1" id="Capa_1" viewBox="0 0 460.775 460.775" ><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" ></g><g id="SVGRepo_iconCarrier"> <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path> </g></svg>
           </div>
           <div className="name" style={{fontSize: '25px', fontWeight: 'bold'}}>
             <div className="inlineCenter" style={{
@@ -220,10 +218,10 @@ const Streaks = () => {
                 display: "inline-flex",
                 alignItems: "center",
                 justifyContent: "center",
-                marginRight: "5px",
+                marginRight: "10px",
                 borderRadius: "50%"
               }}>
-               <svg className="tick" height="10px" width="10px" version="1.1" id="Capa_1" viewBox="0 0 17.837 17.837" fill="#FFFFFF"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M16.145,2.571c-0.272-0.273-0.718-0.273-0.99,0L6.92,10.804l-4.241-4.27 c-0.272-0.274-0.715-0.274-0.989,0L0.204,8.019c-0.272,0.271-0.272,0.717,0,0.99l6.217,6.258c0.272,0.271,0.715,0.271,0.99,0 L17.63,5.047c0.276-0.273,0.276-0.72,0-0.994L16.145,2.571z"></path> </g> </g></svg>
+               <svg className="tick" height="10px" width="10px" version="1.1" id="Capa_1" viewBox="0 0 17.837 17.837" fill="#FFFFFF"><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" ></g><g id="SVGRepo_iconCarrier"> <g> <path d="M16.145,2.571c-0.272-0.273-0.718-0.273-0.99,0L6.92,10.804l-4.241-4.27 c-0.272-0.274-0.715-0.274-0.989,0L0.204,8.019c-0.272,0.271-0.272,0.717,0,0.99l6.217,6.258c0.272,0.271,0.715,0.271,0.99,0 L17.63,5.047c0.276-0.273,0.276-0.72,0-0.994L16.145,2.571z"></path> </g> </g></svg>
               </div>
               <span style={{fontSize: "35px"}}>{streakDisplayName.current}</span>
             </div>
