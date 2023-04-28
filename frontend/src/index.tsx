8195d8f88e697cbd8da49ce735b34252e3ba9a10
yukinoyu <56956339+yukinoyuu@users.noreply.github.com>
2023-04-28 15:31:01 -0500

    frontend restructure (stage 1)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Streaks from './Streaks'
+import Streaks from './components/Streaks'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
